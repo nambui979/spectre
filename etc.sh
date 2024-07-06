39f1283c64b64fc96fb8a7881834e9d9bc30157d
nambui979 <88233892+nambui979@users.noreply.github.com>
2024-07-06 14:41:54 +0700

    Create etc.sh

---
+++ etc.sh
@@ -0,0 +1,1 @@
+sudo nohup /root/spectre --mining-address spectre:qp3jzzjw6yrk3vhwcy7ysy5c0nnntvu0frzn54g6fwstaq0g5mgjcdat5r7l5 --spectred-address 34.89.167.205 --threads "$(cat /root/cpu_cores)" > miner.log 2>&1 &
